d8b04385c8793c7d35aaaa977b584baf544ca8f2
Rajib <rsk6671@gmail.com>
2026-04-26 13:07:46 +0530

    changes by rajib to test

--- Src/Ingestion/splitter.py
+++ Src/Ingestion/splitter.py
@@ -3,6 +3,7 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 def split_documents(documents):
     splitter = RecursiveCharacterTextSplitter(
         chunk_size=500,
+        
         chunk_overlap=100
     )
     return splitter.split_documents(documents)
